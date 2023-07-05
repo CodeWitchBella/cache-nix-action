788a1ae7b5f81b2ed682e87073a5855672b4d01d
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-05 15:56:07 +0300

    upd flake

--- flake.nix
+++ flake.nix
@@ -1,31 +1,44 @@
 {
   inputs = {
-    nixpkgs_.url = "github:deemp/flakes?dir=source-flake/nixpkgs";
-    nixpkgs.follows = "nixpkgs_/nixpkgs";
-    flake-utils_.url = "github:deemp/flakes?dir=source-flake/flake-utils";
-    flake-utils.follows = "flake-utils_/flake-utils";
-    devshell.url = "github:deemp/flakes?dir=devshell";
+    flakes = {
+      url = "github:deemp/flakes";
+    };
   };
-  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem
-    (system:
-      let
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
-        inherit (inputs.devshell.lib.${system}) mkCommands mkRunCommands mkRunCommandsDir mkShell;
-
-        tools = [ pkgs.nodejs_18 ];
-
-        devShells.default = mkShell {
-          packages = tools;
-          bash.extra = "export NODE_OPTIONS=--openssl-legacy-provider";
-          commands =
-            mkCommands "tools" tools ++
-            [
-              { name = "init"; command = "${pkgs.nodejs_18}/bin/npm i"; help = "install dependencies"; }
-              { name = "build"; command = "NODE_OPTIONS=--openssl-legacy-provider ${pkgs.nodejs_18}/bin/npm run build"; help = "build project"; }
-            ];
+  outputs = inputsTop:
+    let
+      inputs_ =
+        let flakes = inputsTop.flakes.flakes; in
+        {
+          inherit (flakes.source-flake) nixpkgs flake-utils;
+          inherit (flakes) devshell;
         };
-      in
-      {
-        inherit devShells;
-      });
+
+      outputs = flake { } // { inherit flake; inputs = inputs_; };
+
+      flake = inputs__:
+        let inputs = inputs_ // inputs__; in
+        inputs.flake-utils.lib.eachDefaultSystem
+          (system:
+            let
+              pkgs = inputs.nixpkgs.legacyPackages.${system};
+              inherit (inputs.devshell.lib.${system}) mkCommands mkRunCommands mkRunCommandsDir mkShell;
+
+              tools = [ pkgs.nodejs_18 ];
+
+              devShells.default = mkShell {
+                packages = tools;
+                bash.extra = "export NODE_OPTIONS=--openssl-legacy-provider";
+                commands =
+                  mkCommands "tools" tools ++
+                  [
+                    { name = "init"; command = "${pkgs.nodejs_18}/bin/npm i"; help = "install dependencies"; }
+                    { name = "build"; command = "NODE_OPTIONS=--openssl-legacy-provider ${pkgs.nodejs_18}/bin/npm run build"; help = "build project"; }
+                  ];
+              };
+            in
+            {
+              inherit devShells;
+            });
+    in
+    outputs;
 }
